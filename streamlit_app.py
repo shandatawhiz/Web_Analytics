86522b7329b9d6bf5f0ba72dc0f6d1b48d505905
Shanmuga Raj N <142706840+shandatawhiz@users.noreply.github.com>
2023-10-04 07:02:40 -0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import atreamlit_analytics
+
+
+with streamlit_analytics.track():
+  st.text_input("Write your name")
+  st.selectbox("Select your favorite", ["Practice","Makes","Succes"])
+  st.button("Click me")
