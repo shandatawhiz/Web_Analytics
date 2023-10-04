b2f46d7d34bd5eab8b521cb60590bea4d16b7df2
Shanmuga Raj N <142706840+shandatawhiz@users.noreply.github.com>
2023-10-04 07:28:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
-pip install streamlit
 import streamlit as st
 import streamlit_analytics
 
 
 with streamlit_analytics.track():
   st.text_input("Write your name")
-  st.selectbox("Select your favorite", ["Practice","Makes","Succes"])
+  st.selectbox("Select your favorite", ["Practice", "Makes", "Succes"])
   st.button("Click me")
